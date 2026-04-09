3d0851ac778f93319ecf0b66258ffa88e75d66df
Harsh <harsh3h3@gmail.com>
2026-04-09 19:16:36 +0530

    Agentic Ai and MCP

---
+++ 9_MCP/fast_MCP.py
@@ -0,0 +1,18 @@
+import random
+from fastmcp import FastMCP
+
+mcp = FastMCP(name = "Demo_Server")
+
+@mcp.tool
+def roll_dice(n_dice : int = 1) -> list[int]:
+    """Roll n_dice 6-sided dice and return the result """
+    return (random.randint(1,6) for _ in range(n_dice))
+
+
+@mcp.tool
+def add_two_no(a :float , b : float) -> float:
+    """Add two numbers and return the answer""" 
+    return a+b
+
+if __name__ == '__main__':
+    mcp.run()
